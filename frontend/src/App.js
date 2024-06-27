2b8bf70454ad66d74ca6814efd625e59b9dd0faf
manik375 <77182423+manik375@users.noreply.github.com>
2024-06-27 08:04:03 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Login/>}/>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<RegisterForm />} />
       </Routes>
